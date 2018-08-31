fadbc79397b8abe5153661d83a82d3ae15e68604
Xiaodong Li <bryne_lxd@sina.com>
2018-09-01 00:33:51 +0800

    demo of func definition and try-except added

--- chapter2/test1.py
+++ chapter2/test1.py
@@ -13,3 +13,23 @@ spam = 0
 while spam < 5:
     print('Hello World!')
     spam += 1
+
+print('======================')
+
+for i in range(5):
+    print('here is ' + str(i))
+
+for i in range(5, -1, -1):
+    print('here i is ' + str(i))
+
+import random
+for i in range(5):
+    print(random.randint(1,10))
+
+print('======================')
+
+print('cats', 'dogs', 'mice')
+
+print('cats', 'dogs', 'mice', sep=',')
+
+print('======================')
