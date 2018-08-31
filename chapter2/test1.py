19a46c8b5ffd708923aaba7b3371ba62e528bb08
Xiaodong Li <bryne_lxd@sina.com>
2018-08-31 21:36:52 +0800

    chapter 1 and 2 first checked in

---
+++ chapter2/test1.py
@@ -0,0 +1,15 @@
+
+name = 'Mike'
+if name == 'Alice':
+    print('Hello, Alice')
+elif name == 'Bob':
+    print('Hello, Bob.')
+else:
+    print('Hi, ' + name)
+
+print('======================')
+
+spam = 0
+while spam < 5:
+    print('Hello World!')
+    spam += 1
